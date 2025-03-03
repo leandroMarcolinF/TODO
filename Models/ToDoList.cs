69b7a229be44733110ec4889d966a95b909086bf
leandroMarcolinF <leandromarcolin3@gmail.com>
2025-03-03 10:15:21 -0300

    Atualizado nomes de atributos mais coerentes e adicionado atributo de Item

--- Models/ToDoList.cs
+++ Models/ToDoList.cs
@@ -7,22 +7,23 @@ namespace TODO.Models
 {
     public class ToDoList
     {
-
-        private bool check = false;
+        private bool allListcheck = false;
         private string title;
-        private string content = "";
+        private string description = "";
+        private List<Item> items;
 
         public ToDoList(string aTitle)
         {
             title = aTitle;
+            items = new();
         }
 
-        public bool Check
+        public bool AllListcheck
         {
-            get { return check;}
+            get { return allListcheck;}
             set {
                 if (value == false) {
-                    check = value;
+                    allListcheck = value;
                 } else {
                     Console.WriteLine("You can not uncheck.");
                 }
@@ -40,6 +41,11 @@ namespace TODO.Models
                     Console.WriteLine("The title is longer than allowed.");
                 }
             }
+        }
+
+        public void CreateItem(string titleItem, bool check = false)
+        {
+            items.Add(new Item(titleItem, check));
         }
     }
 }
