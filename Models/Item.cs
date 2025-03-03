5ed878746da62c86afa847bd7260f91e14555530
leandroMarcolinF <leandromarcolin3@gmail.com>
2025-03-03 10:14:02 -0300

    Criado atributos e métodos iniciais da classe Item.cs

--- Models/Item.cs
+++ Models/Item.cs
@@ -7,6 +7,29 @@ namespace TODO.Models
 {
     public class Item
     {
-        
+        private string title;
+        private bool check;
+        private DateTime dateTime;
+
+        public Item(string aTitle, bool aCheck = false)
+        {
+            UpdateTitle(aTitle);
+            check = aCheck;
+            dateTime = DateTime.Now;
+        }
+
+        private void UpdateTitle(string newTitle)
+        {
+            ValidateTitle(newTitle);
+
+            title = newTitle;
+        }
+
+        private static void ValidateTitle(string title)
+        {
+            if (title.Length > 30) {
+                throw new Exception("The item's title is too long.");
+            }
+        }
     }
 }
